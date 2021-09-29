4624baa156f22838d0f45d95dd99dc8d3b1faf65
Tanner Oliver <435.tanner@gmail.com>
2021-09-29 10:06:02 -0500

    adding the layout for the webpage and creating essential files

---
+++ assets/js/script.js
@@ -0,0 +1,3 @@
+// I need to apply colors to note taking area at apropriate times
+//I need to look into local storage and if anything is in local storage for a certain time I need to apply that text content to appropriate times note taking area
+//when I click the save button on the right side of the screen it needs to save content to local storage
