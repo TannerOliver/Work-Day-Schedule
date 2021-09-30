52b7a521fe7e6c66b79d75faea51e67df4b642fa
Tanner Oliver <435.tanner@gmail.com>
2021-09-29 19:41:11 -0500

    adding some functionality to the page

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,49 @@
-// I need to apply colors to note taking area at apropriate times
-//I need to look into local storage and if anything is in local storage for a certain time I need to apply that text content to appropriate times note taking area
-//when I click the save button on the right side of the screen it needs to save content to local storage
+//state=["", "note", ""]   [9, 10, 11, etc.] make an area for all of the time blocks inside of the timeblock array
+
+//when I save: update the global variable "allTheNotes"
+
+//selectors
+var mainContainer = document.getElementById("container");
+$("#currentDay").text(moment());
+
+//variables
+var allTheNotes = []; //single source of truth
+
+var saveBtn = $(".saveBtn");
+
+//functions
+function init() {
+  //check local storage
+  localStorage.getItem("allTheNotes");
+  console.log(localStorage.getItem("allTheNotes"));
+  //update global variable with local storage
+  //populate the form fields with whatever saved data exists
+  // populate with whatever saved data exits <textarea data-hour="9" /> put this into the text area to make it a specific hour block or to identify that text block
+  // use momentjs to see if the current time vloack is past , present or future
+  //$(this).addClass("") this point to a specific field
+}
+
+//this checks the time and should display the class for the css
+function checkTime() {
+  var hours = moment().format("H");
+  $(".time").each(function () {
+    var currentRow = parseInt($(this).val(".time"));
+    console.log(currentRow);
+    //if (".row")
+  });
+}
+
+//this function runs when user clicks save button
+saveBtn.on("click", function () {
+  $("textarea").each(function () {
+    console.log($(this).val());
+    allTheNotes.push($(this).val());
+  });
+  localStorage.removeItem("allTheNotes");
+  localStorage.setItem("allTheNotes", allTheNotes);
+  console.log("clicked");
+});
+
+//calling functions here
+checkTime();
+init();
