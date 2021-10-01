a43945f28865f621a07d01bf4168444b3c29fe2e
Tanner Oliver <435.tanner@gmail.com>
2021-09-30 19:07:40 -0500

    adding functionality and fixing readme

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# hw05-work-day-scheudler
+## Hw05-work-day-scheudler
+# Objective
+In this homework I built a schedule using local storage to save the data and keep it stored for the next time it is loaded. Using local storage everytime this webpage is loaded up it will display what someone has saved into it. I had a hard time with the momentjs I still have to fidle with it.
+## Screenshot
+![ScheduleScreenshot1](./assets/images/schedule.png)
+## Deployed Page
+https://tanneroliver.github.io/hw05-work-day-scheudler/
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,47 +1,40 @@
-//state=["", "note", ""]   [9, 10, 11, etc.] make an area for all of the time blocks inside of the timeblock array
-
-//when I save: update the global variable "allTheNotes"
-
 //selectors
 var mainContainer = document.getElementById("container");
 $("#currentDay").text(moment());
 
 //variables
-var allTheNotes = []; //single source of truth
-
+var allTheNotes = [];
 var saveBtn = $(".saveBtn");
 
 //functions
 function init() {
-  //check local storage
-  localStorage.getItem("allTheNotes");
-  console.log(localStorage.getItem("allTheNotes"));
-  //update global variable with local storage
-  //populate the form fields with whatever saved data exists
-  // populate with whatever saved data exits <textarea data-hour="9" /> put this into the text area to make it a specific hour block or to identify that text block
-  // use momentjs to see if the current time vloack is past , present or future
-  //$(this).addClass("") this point to a specific field
+  var storedNotes = JSON.parse(localStorage.getItem("allTheNotes"));
+  $(".description").each(function (index) {
+    $(this).val(storedNotes[index]);
+  });
 }
 
 //this checks the time and should display the class for the css
 function checkTime() {
   var hours = moment().format("H");
   $(".time").each(function () {
-    var currentRow = parseInt($(this).val(".time"));
-    console.log(currentRow);
-    //if (".row")
+    var currentRow = parseInt($(this).attr("id"));
+    if (currentRow < hours) {
+      $(".description").addClass("past");
+    } else if (currentRow === hours) {
+      $(".description").addClass("present");
+    } else currentRow > hours;
+    $(".description").addClass("future");
   });
 }
 
 //this function runs when user clicks save button
 saveBtn.on("click", function () {
   $("textarea").each(function () {
-    console.log($(this).val());
     allTheNotes.push($(this).val());
   });
   localStorage.removeItem("allTheNotes");
-  localStorage.setItem("allTheNotes", allTheNotes);
-  console.log("clicked");
+  localStorage.setItem("allTheNotes", JSON.stringify(allTheNotes));
 });
 
 //calling functions here
